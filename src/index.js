bc3c4b27bceb8907b209a55b6ed59790a1aaa242
Daniel Wilfredo Bonilla Robles <danydanny1705@gmail.com>
2020-05-23 11:17:23 -0600

    configuration redux, react router dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import { Provider } from 'react-redux';
+import moment from 'moment';
+import es_ES from 'antd/es/locale-provider/es_ES';
+import 'moment/locale/es';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
+
+moment.locale('es');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <ConfigProvider locale={es_ES}>
+      <Router>
+        <App />
+      </Router>
+    </ConfigProvider>
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
